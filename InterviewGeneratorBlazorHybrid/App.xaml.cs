bf78fd5d44e98883ddd0fff85e8f2b3f55c2f04a
John van Dijk <john@johnv.ca>
2025-10-25 05:50:46 -0400

    Change app window title.

--- InterviewGeneratorBlazorHybrid/App.xaml.cs
+++ InterviewGeneratorBlazorHybrid/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "InterviewGeneratorBlazorHybrid" };
+            return new Window(new MainPage()) { Title = "Interview Generator" };
         }
     }
 }
