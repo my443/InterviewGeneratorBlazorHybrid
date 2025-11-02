48c83cbb92264878ad7aa534da236511a82a58b6
John van Dijk <john@johnv.ca>
2025-11-02 13:40:43 -0500

    Fixed the interviews buttons some more. Now it is working as expected.

---
+++ InterviewGeneratorBlazorHybrid/Components/Shared/NavButtons.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewGeneratorBlazorHybrid.Components.Shared
+{
+    public partial class NavButtons
+    {
+        public void GoToInterviewsList()
+        {
+            ViewModel.ResetForm();
+            Navigation.NavigateTo($"/interviews");
+        }
+    }
+}
